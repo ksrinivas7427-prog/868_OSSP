bb6ffc3dc1874e2a08ef89c34f35f8fba383c798
Srinivas <k.srinivas7427@gmail.com>
2026-09-09 05:27:40 +0000

    added week programs

---
+++ 2520030146_Practical/pipe.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+ int pid;
+ pid = fork();
+ if (pid < 0) {
+    printf("Failure in process creation\n");
+    exit(0);
+ }
+ else if (pid == 0) {
+    printf("I am in Child Process\n");
+ }
+ else {
+    printf("I am in Parent Process\n");
+ }
+ return 0;
+}
